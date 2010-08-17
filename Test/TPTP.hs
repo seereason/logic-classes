e2430f18449386017a1b4b809c9d7c126f999c21
David Fox <dsf@seereason.com>
2010-08-17 21:58:37 +0000

    Add Test.TPTP

---
+++ Test/TPTP.hs
@@ -0,0 +1,22 @@
+module Test.TPTP where
+    
+import Codec.TPTP (Formula)
+import Logic.FirstOrder (conj)
+import Logic.Instances.TPTP
+import Logic.Monad (runNormal)
+import Logic.Logic (Logic ((.~.), (.=>.)))
+import Logic.NormalForm (cnfTrace)
+import Test.Data (chang43KB, chang43Conjecture)
+import Test.HUnit
+import Test.Types (TestFormula(formula))
+
+tests :: Test
+tests = TestLabel "TPTP" $ TestList [tptp]
+
+tptp :: Test
+tptp =
+    TestCase (assertEqual "tptp cnf trace" "" (runNormal (cnfTrace f)))
+    where
+      f :: Formula
+      f = (.~.) (conj (map formula (snd (chang43KB :: (String, [TestFormula Formula])))) .=>.
+                 formula chang43Conjecture)
