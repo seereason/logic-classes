2b6dd5836e326aee547ce881298b835211d8e906
David Fox <dsf@seereason.com>
2010-08-21 13:39:14 +0000

    Update some superclasses and function names in Logic.Satisfiable

--- Logic/Satisfiable.hs
+++ Logic/Satisfiable.hs
@@ -11,28 +11,30 @@ module Logic.Satisfiable
     , invalid
     ) where
 
+import qualified Data.Set as S
+import Logic.Clause (Literal)
 import Logic.FirstOrder (FirstOrderLogic(..), toPropositional)
 import Logic.Logic ((.~.))
 import Logic.Monad (NormalT)
-import Logic.NormalForm (clausalNormalForm)
+import Logic.NormalForm (clauseNormalForm)
 import Logic.Instances.PropLogic ()
 import qualified PropLogic as PL
 
-satisfiable :: (Monad m, FirstOrderLogic formula term v p f, Ord formula) =>
+satisfiable :: (Monad m, FirstOrderLogic formula term v p f, Literal formula, Ord formula) =>
                 formula -> NormalT v term m Bool
 satisfiable f = clauses f >>= return . PL.satisfiable
 
-clauses :: (Monad m, FirstOrderLogic formula term v p f) => formula -> NormalT v term m (PL.PropForm formula)
-clauses f = clausalNormalForm f >>= return . PL.CJ . map (PL.DJ . map (toPropositional PL.A))
+clauses :: (Monad m, FirstOrderLogic formula term v p f, Literal formula) => formula -> NormalT v term m (PL.PropForm formula)
+clauses f = clauseNormalForm f >>= return . PL.CJ . map (PL.DJ . map (toPropositional PL.A)) . map S.toList . S.toList
 
-inconsistant :: (Monad m, FirstOrderLogic formula term v p f, Ord formula) =>
+inconsistant :: (Monad m, FirstOrderLogic formula term v p f, Literal formula, Ord formula) =>
                 formula -> NormalT v term m Bool
 inconsistant f =  satisfiable f >>= return . not
 
-theorem :: (Monad m, FirstOrderLogic formula term v p f, Ord formula) =>
+theorem :: (Monad m, FirstOrderLogic formula term v p f, Literal formula, Ord formula) =>
            formula -> NormalT v term m Bool
 theorem f = inconsistant ((.~.) f)
 
-invalid :: (Monad m, FirstOrderLogic formula term v p f, Ord formula) =>
+invalid :: (Monad m, FirstOrderLogic formula term v p f, Literal formula, Ord formula) =>
            formula -> NormalT v term m Bool
 invalid f = inconsistant f >>= \ fi -> theorem f >>= \ ft -> return (not (fi || ft))
