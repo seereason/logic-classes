9784e7df27102513afcf5dcf93ecc4d89c4dc9be
David Fox <dsf@seereason.com>
2015-04-15 08:22:42 -0700

    Remove ad-hoc Setup.hs code to run test script

--- Setup.hs
+++ Setup.hs
@@ -3,19 +3,8 @@
 module Main where
 
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
-import Distribution.Simple.Program
-import System.Cmd
 import System.Directory (copyFile)
-import System.Exit
 
 main :: IO ()
 main = copyFile "debian/changelog" "changelog" >>
-       defaultMainWithHooks simpleUserHooks {
-         postBuild = \ _ _ _ lbi -> runTestScript lbi
-       , runTests = \ _ _ _ lbi -> runTestScript lbi
-       }
-
-runTestScript lbi =
-    system (buildDir lbi ++ "/tests/tests") >>= \ code ->
-    if code == ExitSuccess then return () else error "Test Failure"
+       defaultMainWithHooks simpleUserHooks
